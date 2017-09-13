d3e9e2f7aaaa8b69b6429c504be4961a56366bcd
Alex Wiese <aleX@wiese.com.au>
2017-09-13 10:58:36 +0930

    Made usings pretty
    As per Hanselman aesthetics requirements.

--- src/YouTubePlaylistCopier/Pages/Index.cshtml.cs
+++ src/YouTubePlaylistCopier/Pages/Index.cshtml.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Google;
-using Google.Apis.YouTube.v3.Data;
 using Google.Apis.YouTube.v3;
-using Google.Apis.Services;
 using Google.Apis.Auth.OAuth2;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication;
+using Google.Apis.YouTube.v3.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Google.Apis.Services;
 using Google;
 
 namespace YouTubePlaylistCopier.Pages
